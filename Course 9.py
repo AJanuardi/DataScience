8856044f18bebe82922a5e0f1a0fee1562ccfa1b
AJanuardi <59459425+AJanuardi@users.noreply.github.com>
2020-09-16 19:01:57 +0700

    Add files via upload

---
+++ Course 9.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# # This is the first notebook of Applied Data Science Capstone Course for Peer-graded Assignment: Capstone Project Notebook of Week 1
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
